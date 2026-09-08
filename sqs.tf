846efa3e18d0835a7cb635c905178b66f0e9b1ff
Kenneth Gonzalez <kennethgonzalez067@gmail.com>
2026-09-08 12:03:18 -0400

    adding DLQ

--- sqs.tf
+++ sqs.tf
@@ -9,6 +9,31 @@ resource "aws_sqs_queue" "terraform_sqs_queue" {
   tags = {
     Environment = "dev"
   }
+    // If message is not processed after 3 attempts, send it to the dead letter queue.
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.terraform_sqs_dead_letter_queue.arn
+    maxReceiveCount     = 3
+  })
+}
+
+resource "aws_sqs_queue" "terraform_sqs_dead_letter_queue" {
+  name                      = "terraform-example-dead-letter-queue"
+  delay_seconds             = 0
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+
+  tags = {
+    Environment = "dev"
+  }
+}
+// Allow the DLQ to receive messages from the main queue
+resource "aws_sqs_queue_redrive_allow_policy" "allow_redrive" {
+  queue_url = aws_sqs_queue.terraform_sqs_dead_letter_queue.id
+  redrive_allow_policy = jsonencode({
+    redrivePermission = "byQueue"
+    sourceQueueArns   = [aws_sqs_queue.terraform_sqs_queue.arn]
+  })
 }
 
 // Policy for Lambda role to send messages to SQS
